d0fb40ce26a8ae6ccac4a3d50e47ca2c57137461
Palm <tauman.exe@gmail.com>
2025-08-02 20:23:21 +0200

    Reset the car if it goes off course, speedometer for extra fun

--- game/game.gd
+++ game/game.gd
@@ -9,7 +9,6 @@ signal lap_finished(next_lap)
 func _ready() -> void:
 	generate_checkpoints()
 	place_car()
-	
 
 func generate_checkpoints() -> void:
 	checkpoints = []
@@ -17,10 +16,11 @@ func generate_checkpoints() -> void:
 		checkpoints.append(cp.global_position)
 
 func place_car() -> void:
-	var spawn = (checkpoints[0] + checkpoints[-1]) / 2 + Vector3.UP
+	var spawn = (checkpoints[current_cp] + checkpoints[current_cp-1]) / 2 + Vector3.UP
 	car.global_position = spawn
 	car.get_node("RigidBody3D").global_position = spawn
-	car.look_at(checkpoints[0], Vector3.UP)
+	car.get_node("RigidBody3D").linear_velocity = Vector3.ZERO
+	car.look_at(checkpoints[current_cp], Vector3.UP)
 
 func _process(_delta: float) -> void:
 	var distance_to_current_cp = (car.global_position - checkpoints[current_cp]).length_squared()
@@ -29,8 +29,16 @@ func _process(_delta: float) -> void:
 		current_cp = (current_cp + 1) % checkpoints.size()
 		if current_cp == 0:
 			new_lap()
+	if player_is_off_course():
+		place_car()
+	if Input.is_action_just_pressed("Reset"):
+		place_car()
 
 func new_lap() -> void:
 	current_lap += 1
 	lap_finished.emit(current_lap)
-	
+
+func player_is_off_course() -> bool:
+	var tolerance = 60.0 * 60.0
+	var distance_to_last_cp = (car.global_position - checkpoints[current_cp]).length_squared()
+	return distance_to_last_cp > tolerance
