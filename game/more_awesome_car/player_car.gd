1e947ffe7ce6176d6b3df45fbb95130e0638be3c
DESKTOP-AN1TC24\Navin Brohier <navin.brohier@gmail.com>
2025-08-03 02:46:44 +1000

    Changes:
    todo funcs for flipping / reseting car.

--- game/more_awesome_car/player_car.gd
+++ game/more_awesome_car/player_car.gd
@@ -8,21 +8,32 @@ extends VehicleBody3D
 @onready var camera_3d = $CameraPivot/Camera3D
 @export var Camera_Follow_Speed := 20.0
 
-var look_at
+var look_at_camera
+
+var last_checkpoint
+signal hit_checkpoint(checkpoint)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	look_at = global_position
-
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	look_at_camera = global_position
+	hit_checkpoint.connect(setLastCheckpoint)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if(Input.is_action_just_pressed("Quit")):get_tree().quit()
-	if(Input.is_action_just_pressed("Reset")):rotation = Vector3.ZERO
+	if(Input.is_action_just_pressed("Reset")):rotation = Vector3.ZERO #replace with unstuck
 	steering = move_toward(steering,Input.get_axis("right","left")* MAX_STEER, delta* 2.5)
 	engine_force = Input.get_axis("break","accelerate") * ENGINE_POWER #applied per traction wheel
 	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position,delta * Camera_Follow_Speed)
 	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform,delta * 5.0)
-	look_at = look_at.lerp(global_position + linear_velocity,delta * 5.0)
-	camera_3d.look_at(look_at)
+	look_at_camera = look_at_camera.lerp(global_position + linear_velocity,delta * 5.0)
+	camera_3d.look_at(look_at_camera)
+
+func setLastCheckpoint(new_checkpoint):
+	last_checkpoint = new_checkpoint
+	print("Checkpoint set")
+	
+func Unstuck():
+	global_transform = last_checkpoint.global_transform
+	print("Returned to last checkpoint")
