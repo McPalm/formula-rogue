4cbca3b8ab9451fd455e7398e6892e5a7b2fdc3c
Palm <tauman.exe@gmail.com>
2025-08-02 12:40:27 +0200

    a

--- game/camera.gd
+++ game/camera.gd
@@ -1,11 +1,16 @@
-extends Camera3D
+extends Node3D
 
 @export var focus:Node3D
 @export var car:Node3D
+@export var camera:Camera3D
+
+func _ready() -> void:
+	camera.top_level = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	fov = lerpf(60, 90, car.speed / car.top_speed)
+	camera.fov = lerpf(60, 90, car.speed / car.top_speed)
 	position.x = car.drift / -7.0
 	look_at(focus.global_position, car.global_basis.y)
-	
+	camera.global_basis = camera.global_basis.slerp(global_basis, 0.3)
+	camera.global_position = camera.global_position.lerp(global_position, 0.3)
--- game/tracks/path.gd
+++ game/tracks/path.gd
@@ -5,3 +5,8 @@ extends Node3D
 func _ready() -> void:
 	for n in get_child_count():
 		var area:Area3D = get_child(n).get_child(1)
+		area.body_entered.connect(touch_point.bind(n))
+
+func touch_point(body:Node3D, p:int) -> void:
+	if body is RigidBody3D:
+		print(p)
